ed45102c4656e05062deeb1e59c611ed863572bf
k-acheampong <kwameacheampong1@gmail.com>
2021-02-26 16:19:14 -0500

    inital commit

--- item.py
+++ item.py
@@ -1,6 +1,6 @@
 class Items():
 
-    def __init__(name, cost, price, amount, perishable):
+    def __init__(self, name, cost, price, amount, perishable):
         self.name = name
         self.cost = cost
         self.price = price
@@ -10,10 +10,10 @@ class Items():
     def __str__(self):
         return "Item: {}, {}, {}, {}, {}".format(self.name, self.cost, self.price, self.amount, self.perishable)
 
-    def recieve_shipment(x):
+    def recieve_shipment(self, x):
         self.amount = self.amount + x
         return self.amount
 
-    def sold(x):
+    def sold(self, x):
         self.amount = self.amount - x
         return self.amount
