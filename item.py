87c7a89f62e137ad8e0a096bf45e8aed46520967
k-acheampong <kwameacheampong1@gmail.com>
2021-02-26 17:39:49 -0500

    added getters

--- item.py
+++ item.py
@@ -11,6 +11,24 @@ class Item():
     def __str__(self):
         return "Item: {}, {}, {}, {}, {}, {}".format(self.id, self.name, self.cost, self.price, self.amount, self.perishable)
 
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.name
+
+    def get_cost(self):
+        return self.cost
+
+    def get_price(self):
+        return self.price
+
+    def get_amount(self):
+        return self.amount
+
+    def get_perishable(self):
+        return self.perishable
+
     def recieve_shipment(self, x):
         self.amount = self.amount + x
         return self.amount
