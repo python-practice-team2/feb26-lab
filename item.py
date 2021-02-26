66a372973fec51707e352bf11caf1c847ccedba8
kelbymartin <kelbymartin65@gmail.com>
2021-02-26 17:04:26 -0500

    Added item id

--- item.py
+++ item.py
@@ -1,6 +1,7 @@
-class Items():
+class Item():
 
-    def __init__(self, name, cost, price, amount, perishable):
+    def __init__(self, id, name, cost, price, amount, perishable):
+        self.id = id
         self.name = name
         self.cost = cost
         self.price = price
@@ -8,7 +9,7 @@ class Items():
         self.perishable = perishable
 
     def __str__(self):
-        return "Item: {}, {}, {}, {}, {}".format(self.name, self.cost, self.price, self.amount, self.perishable)
+        return "Item: {}, {}, {}, {}, {}, {}".format(self.id, self.name, self.cost, self.price, self.amount, self.perishable)
 
     def recieve_shipment(self, x):
         self.amount = self.amount + x
